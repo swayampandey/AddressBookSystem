e767e79ba689da4baaf5608e88d108ed76dbd1a3
Swayam Prakash <swayam265@gmail.com>
2022-09-16 21:10:47 +0530

    [Swayam] : added functionality to create contact in address book program

---
+++ src/com/bridelabz/Contact.java
@@ -0,0 +1,69 @@
+package com.bridelabz;
+
+public class Contact {
+	
+	private String firstName;
+	private String lastName;
+	private String Address; 
+	private String city;
+	private String state;
+	private int zip;
+	private long phoneNum;
+	private String emailId;
+	
+	/*
+	 *  assigning the values to variable using setter method 
+	 *  taking the values from variable using getter method
+	 */
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public long getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(long phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+
+}
